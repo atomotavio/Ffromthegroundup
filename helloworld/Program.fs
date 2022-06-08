8b5e5b7ecd15aa2c7b8115cedc639b0dcf1a780a
atomotavio <atomotavio@gmail.com>
2022-06-08 11:19:09 -0300

    Initial commit

---
+++ helloworld/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+[<EntryPoint>]
+let main argV =
+    let Person =
+        if argV.Length > 0 then
+            argV.[0]
+        else 
+            "Otávio"
+    printf "Hello %s from F#!" Person
+    0
